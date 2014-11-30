5ae7e3a3e7e17700579c5009359bf86802215209
tehreemb <tehreemb@gmail.com>
2014-11-30 16:05:47 +0000

    Update downloadftpfile.ps1

--- downloadftpfile.ps1
+++ downloadftpfile.ps1
@@ -7,9 +7,9 @@ $webclient = New-Object System.Net.WebClient
 $webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
 
 $source1 =  "ftp://ftp.company.com/Data/Raw data dump/dailyreport20141114.xlsx"
-$target1 = '\\corporatenetwork\DailyData\FTPSAIN\dailyreport20141114.xlsx'
+$target1 = '\\corporatenetwork\DailyData\FTPIN\dailyreport20141114.xlsx'
 
 $WebClient.DownloadFile($source1, $target1)
 
 #copies and renames the file you have just downloaded (takes out the date appended to the filename) via FTP to a new folder that you can point SSIS or reporting tool to
-Copy-Item '\\corporatenetwork\DailyData\FTPSAIN\dailyreport*.xlsx' '\\corporatenetwork\DailyData\FTPSAIN\dailyreport.xlsx'
+Copy-Item '\\corporatenetwork\DailyData\FTPIN\dailyreport*.xlsx' '\\corporatenetwork\DailyData\FTPOUT\dailyreport.xlsx'
